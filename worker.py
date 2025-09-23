eabfd95a54697fe41c3b68a94c641fdea78d52d0
Nicolas <nruizu@eafit.edu.co>
2025-09-23 10:03:52 -0500

    deployment_2.2

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from itertools import groupby
 from dotenv import load_dotenv
 import hashlib
 
-load_dotenv()
+load_dotenv("/home/ubuntu/.env")
 
 MASTER = os.environ["MASTER_URL"]
 WORKER_ID = os.environ["WORKER_ID"]
